55c582417abda1201e2f2b4c84547fa5a3ea1b8e
romannawaz <romannawaz01@gmail.com>
2022-05-06 20:59:11 +0300

    feat: services module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Services Module
+import { ServicesModule } from './services/services.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServicesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
