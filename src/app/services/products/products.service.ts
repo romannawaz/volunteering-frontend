dcaf890ce1193f6e33379deb9654da5ffaa2c41a
romannawaz <romannawaz01@gmail.com>
2022-05-07 21:13:34 +0300

    feat(products.service): create product method

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { Product, ProductsServiceInterface } from './produts.service.interface';
 
@@ -20,5 +21,13 @@ export class ProductsService implements ProductsServiceInterface {
 
   public getAllProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.windowService.endpointApi()}/products`);
+  }
+
+  public createProduct(product: Product): Observable<Product> {
+    return this.http
+      .post<Product>(`${this.windowService.endpointApi()}/products`, product)
+      .pipe(
+        take(1)
+      );
   }
 }
