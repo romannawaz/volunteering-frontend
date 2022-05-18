98b0ddd21ee6062ef900b6fabce1e384d736534d
romannawaz <romannawaz01@gmail.com>
2022-05-18 18:48:37 +0300

    feat(auth-component): reroute home if logged

--- src/app/pages/auth/auth.components.ts
+++ src/app/pages/auth/auth.components.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { AuthServiceInterface } from 'src/app/services/auth/auth.service.interface';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _subscription: Subscription = new Subscription();
+
+  constructor(
+    @Inject('AuthServiceInterface')
+    private authService: AuthServiceInterface,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this._subscription.add(
+      this.authService.isLoggedObservable
+        .subscribe(isLogged => {
+          if (isLogged) {
+            this.router.navigateByUrl('/home');
+          }
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
   }
 
 }
