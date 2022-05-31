962ab33c27afc59eda8d4f935cc36e3cbe031c51
romannawaz <romannawaz01@gmail.com>
2022-05-31 21:30:54 +0300

    feat(product-preview): getting user

--- src/app/components/product-preview/product-preview.component.ts
+++ src/app/components/product-preview/product-preview.component.ts
@@ -1,21 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription } from 'rxjs';
 
 // Interfaces
 import { Product } from 'src/app/services/products/product.interface';
+import { User } from 'src/app/services/auth/user.interface';
+
+// Services
+import { AuthServiceInterface } from 'src/app/services/auth/auth.service.interface';
 
 @Component({
   selector: 'product-preview',
   templateUrl: './product-preview.component.html',
   styleUrls: ['./product-preview.component.scss']
 })
-export class ProductPreviewComponent implements OnInit {
+export class ProductPreviewComponent implements OnInit, OnDestroy {
+  private _subscription: Subscription = new Subscription();
 
   @Input()
   public product!: Product;
 
-  constructor() { }
+  public user!: User;
+  public isUserLoaded: boolean = false;
+
+  constructor(
+    @Inject('AuthServiceInterface')
+    private authService: AuthServiceInterface,
+  ) { }
 
   ngOnInit(): void {
+    this._subscription.add(
+      this.authService.getUserById(this.product.user_id)
+        .subscribe((user: User) => {
+          this.user = user;
+
+          this.isUserLoaded = true;
+        })
+    )
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
   }
 
 }
