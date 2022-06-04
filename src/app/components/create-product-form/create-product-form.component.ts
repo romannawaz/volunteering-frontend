0eca8699ae9d836da3d5ed3d9bdfa4de3a26b865
romannawaz <romannawaz01@gmail.com>
2022-06-04 13:30:31 +0300

    feat(create-product-form): now can update product

--- src/app/components/create-product-form/create-product-form.component.ts
+++ src/app/components/create-product-form/create-product-form.component.ts
@@ -1,25 +1,35 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
 
+import { Subscription } from 'rxjs';
+
+// Models
 import { ProductModel } from 'src/app/services/products/models/product.model';
+
+// Interfaces
+import { Product } from 'src/app/services/products/product.interface';
 
 // Services
 import { AuthServiceInterface } from 'src/app/services/auth/auth.service.interface';
 import { ProductsServiceInterface } from 'src/app/services/products/products.service.interface';
-
-// Interfaces
-import { Product } from 'src/app/services/products/product.interface';
 
 @Component({
   selector: 'create-product-form',
   templateUrl: './create-product-form.component.html',
   styleUrls: ['./create-product-form.component.scss']
 })
-export class CreateProductFormComponent implements OnInit {
+export class CreateProductFormComponent implements OnInit, OnChanges, OnDestroy {
+  private _subscription: Subscription = new Subscription();
 
-  public createProductForm!: FormGroup;
+  @Input()
+  public editableProduct?: Product;
+  public editStatus: boolean = false;
+
+  public productForm!: FormGroup;
 
   public controls!: { [key: string]: AbstractControl };
+
+  public isResponseLoading: boolean = false;
 
   constructor(
     @Inject('ProductsServiceInterface')
@@ -30,30 +40,69 @@ export class CreateProductFormComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.createProductForm = this._createForm();
+    this.productForm = this._createProductForm();
 
     this.controls = {
-      title: this.createProductForm.controls['title'],
-      description: this.createProductForm.controls['description'],
-      price: this.createProductForm.controls['price'],
+      title: this.productForm.controls['title'],
+      description: this.productForm.controls['description'],
+      price: this.productForm.controls['price'],
+    }
+  }
+
+  ngOnChanges(): void {
+    if (!this.editableProduct) {
+      this.editStatus = false;
+
+      return;
+    }
+
+    this.productForm.patchValue(this.editableProduct);
+    this.editStatus = true;
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
+
+  public createProduct(): void {
+    if (!this.productForm.valid) return;
+
+    this.isResponseLoading = true;
+
+    const { title, description, price } = this.productForm.value;
+
+    const newProduct: Product = new ProductModel(title, description, price, this.authService.user!._id);
+
+    if (this.editStatus) {
+      this._subscription.add(
+        this.productsService.updateProduct(this.editableProduct!._id!, newProduct)
+          .subscribe(
+            () => {
+              this.productsService.replaceProduct(Object.assign({ id: this.editableProduct!._id }, newProduct));
+
+              this.productForm.reset();
+              this.isResponseLoading = false;
+            }
+          )
+      );
+    }
+    else {
+      this._subscription.add(
+        this.productsService.createProduct(newProduct)
+          .subscribe(
+            (product: Product) => {
+              this.productsService.currentUsersProducts = product;
+
+              this.productForm.reset();
+              this.isResponseLoading = false;
+            }
+          )
+      );
     }
 
   }
 
-  public createProduct(): void {
-    if (!this.createProductForm.valid) return;
-
-    const { title, description, price } = this.createProductForm.value;
-
-    const newProduct: Product = new ProductModel(title, description, price, this.authService.user!._id);
-
-    this.productsService.createProduct(newProduct)
-      .subscribe();
-
-    this.createProductForm.reset();
-  }
-
-  private _createForm(): FormGroup {
+  private _createProductForm(): FormGroup {
     return this.formBuilder.group({
       title: [''],
       description: [''],
