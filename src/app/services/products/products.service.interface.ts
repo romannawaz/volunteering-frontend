c69a75e951c740ab5e3d19d0a7070debf7cb7ffc
romannawaz <romannawaz01@gmail.com>
2022-06-04 13:28:24 +0300

    feat(product.service): added subject and update, delete methods

--- src/app/services/products/products.service.interface.ts
+++ src/app/services/products/products.service.interface.ts
@@ -3,8 +3,18 @@ import { Observable } from "rxjs";
 import { Product } from "./product.interface";
 
 export interface ProductsServiceInterface {
+    readonly $currentUsersProducts: Observable<Product[]>;
+
+    get getCurrentUsersProducts(): Product[];
+    set currentUsersProducts(product: Product);
+
     getAllProducts(): Observable<Product[]>;
     getProductById(id: string): Observable<Product>;
     getProductsByUserId(user_id: string): Observable<Product[]>
     createProduct(product: Product): Observable<Product>;
+    updateProduct(id: string, product: Product): Observable<Product>
+    deleteProduct(id: string): Observable<Product>;
+
+    replaceProduct(newProduct: Product): void;
+    removeProduct(removedProduct: Product): void;
 }
--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -1,20 +1,24 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
+
+// Interfaces
+import { Product } from './product.interface';
 
 // Services
 import { ProductsServiceInterface } from './products.service.interface';
 import { WindowServiceInterface } from '../window/window.service.interface';
 
-// Interfaces
-import { Product } from './product.interface';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService implements ProductsServiceInterface {
+  private readonly _currentUsersProducts: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+  public readonly $currentUsersProducts: Observable<Product[]> = this._currentUsersProducts.asObservable();
+
+  private productsEndpoint: string = `${this.windowService.endpointApi()}/products`;
 
   constructor(
     @Inject('WindowServiceInterface')
@@ -22,23 +26,61 @@ export class ProductsService implements ProductsServiceInterface {
     private http: HttpClient,
   ) { }
 
+  public get getCurrentUsersProducts(): Product[] {
+    return this._currentUsersProducts.getValue();
+  }
+
+  public set currentUsersProducts(newProduct: Product) {
+    const currensUsersProducts: Product[] = [...this.getCurrentUsersProducts];
+
+    if (currensUsersProducts.find(product => product._id == newProduct._id)) return;
+
+    currensUsersProducts.push(newProduct);
+    this._currentUsersProducts.next(currensUsersProducts);
+  }
+
   public getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.windowService.endpointApi()}/products`);
+    return this.http.get<Product[]>(this.productsEndpoint);
   }
 
   public getProductById(id: string): Observable<Product> {
-    return this.http.get<Product>(`${this.windowService.endpointApi()}/products/${id}`);
+    return this.http.get<Product>(`${this.productsEndpoint}/${id}`);
   }
 
   public getProductsByUserId(user_id: string): Observable<Product[]> {
-    return this.http.get<Product[]>(`${this.windowService.endpointApi()}/products/user/${user_id}`);
+    return this.http.get<Product[]>(`${this.productsEndpoint}/user/${user_id}`);
   }
 
   public createProduct(product: Product): Observable<Product> {
     return this.http
-      .post<Product>(`${this.windowService.endpointApi()}/products`, product)
+      .post<Product>(this.productsEndpoint, product)
       .pipe(
         take(1)
       );
+  }
+
+  public updateProduct(id: string, product: Product): Observable<Product> {
+    return this.http
+      .put<Product>(`${this.productsEndpoint}/${id}`, product);
+  }
+
+  public deleteProduct(id: string): Observable<Product> {
+    return this.http
+      .delete<Product>(`${this.productsEndpoint}/${id}`);
+  }
+
+  public replaceProduct(newProduct: Product): void {
+    const currentUsersProducts: Product[] = [...this.getCurrentUsersProducts];
+    const productIndex: number = currentUsersProducts.findIndex((product: Product) => product._id == newProduct._id);
+
+    currentUsersProducts.splice(productIndex, 1, newProduct);
+
+    this._currentUsersProducts.next(currentUsersProducts);
+  }
+
+  public removeProduct(removedProduct: Product): void {
+    const currentUsersProducts: Product[] = [...this.getCurrentUsersProducts].filter((product: Product) => product._id != removedProduct._id);
+
+    this._currentUsersProducts.next(currentUsersProducts);
   }
 }
