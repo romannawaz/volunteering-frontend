8e3488ae6db0125927d3cfd691cf4f5b5aa4888c
romannawaz <romannawaz01@gmail.com>
2022-06-18 21:43:44 +0300

    feat(auth-cabinet): temporary log out button

--- src/app/pages/user-cabinet/user-cabinet.component.ts
+++ src/app/pages/user-cabinet/user-cabinet.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { AuthServiceInterface } from 'src/app/services/auth/auth.service.interface';
 
 @Component({
   selector: 'app-user-cabinet',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCabinetComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject('AuthServiceInterface')
+    public authService: AuthServiceInterface
+  ) { }
 
   ngOnInit(): void {
   }
